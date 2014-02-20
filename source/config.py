b3b32a2a83f68e5c47df69270d4f508b0c21ab19
diego.pacheco <diego.pacheco.it@gmail.com>
2014-02-20 18:54:58 -0300

    changes to make it as they way i wanted.
    changes to make it as they way i wanted.

--- source/config.py
+++ source/config.py
@@ -1,6 +1,6 @@
 # Which boards / lists to count
-BOARDS = ['supply tax']
-LISTS = ['ready']
+BOARDS = ['supply tax','icatu - celula','icatu - namedida mobile','ilegra: bradesco - wallet','jd baseline','thomson board - cortellis','visionboards - phase 1','vendita']
+LISTS = ['code review']
 
 CONSUMER_KEY = '5c212aec85d915429623d7dc30c7c412'
 CONSUMER_SECRET = '456e7f137ad7502d2b069b5c48f6fcf96aa9cee3751ce8d6f6e39a321f1a3c78'
--- source/script.py
+++ source/script.py
@@ -2,10 +2,21 @@
 from trollop import TrelloConnection
 from auth import *
 
+import sys
+
+print 'Extracting pending Code Reviews per projets. \n'
 trello = TrelloConnection(CONSUMER_KEY, authenticate()['oauth_token'])
 
+total_pending = 0
+sys.stdout.write( '## Pending Code Review: \n' )
 for board in filter(lambda b: b.name.lower() in BOARDS, trello.me.boards):
-	print '# %s' % board.name
-	for l in filter(lambda l: l.name.lower() in LISTS, board.lists):
-		print '%s:%i' % (l.name,len(l.cards))
+	sys.stdout.write( board.name + ':')
+	allcards = filter(lambda l: l.name.lower() in LISTS, board.lists)
 
+	if allcards != []:
+		for l in allcards:
+			sys.stdout.write( ' ' + str(len(l.cards)) + '\n' )
+			total_pending = total_pending + len(l.cards)
+	else:
+		sys.stdout.write(' 0\n')
+print '## Total Pending Code Review: %i' % total_pending
